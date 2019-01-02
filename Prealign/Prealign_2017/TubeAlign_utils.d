a94ddb108b3399cdee3194f97466611596dee885
Axel Ekman <axel.a.ekman@gmail.com>
2019-01-02 13:46:45 -0800

    cleanup

--- Prealign/Prealign_2017/TubeAlign_utils.d
+++
@@ -1,9 +0,0 @@
-Prealign_2017/TubeAlign_utils.o: Prealign_2017/TubeAlign_utils.cpp \
- Prealign_2017/TubeAlign_utils.h Prealign_2017/./NCXT-image/image2d.h \
- Prealign_2017/mpfit.h
-
-Prealign_2017/TubeAlign_utils.h:
-
-Prealign_2017/./NCXT-image/image2d.h:
-
-Prealign_2017/mpfit.h:
--- Prealign/Prealign_2017/mpfit.d
+++
@@ -1,3 +0,0 @@
-Prealign_2017/mpfit.o: Prealign_2017/mpfit.c Prealign_2017/mpfit.h
-
-Prealign_2017/mpfit.h:
