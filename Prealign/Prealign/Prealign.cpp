9b5f0b63e247d60e69bf789c7291dc29bea04721
Axel Ekman <axel.ekman@iki.fi>
2023-07-21 12:17:34 +0300

    CLI
    update CLI to cxxopt 5.2.4

--- Prealign/Prealign/Prealign.cpp
+++ Prealign/Prealign/Prealign.cpp
@@ -25,71 +25,92 @@
 #include "mpfit.h"
 
 #include "cxxopts.hpp"
-
-
 using namespace std;
 
-cxxopts::options cxxopts_setup(char *const argv[]) {
-    try {
-        cxxopts::options options(argv[0], "One line description of MyProgram");
+template<typename T>
+void print_arg(cxxopts::parse_result result, std::string tag, std::string desc)
+{
+	int const printwidth = 30;
+	if (result.count(tag)) {
 
-        options.positional_help("projections.mrc angles.rawtlt output.mrc").show_positional_help();
+		std::cout << std::setw(printwidth)
+			<< desc << " = " << result[tag].as<T>() << '\n';
+	}
+	else
+	{
+		std::cout << std::setw(printwidth)
+			<< desc << " = " << result[tag].as<T>() << " (default)\n";
+	}
+}
 
-        options.add_options() //
-            ("positional",
-             "Input files in the order\n"
-             "  projections.mrc: input stack\n"
-             "  angles.rawtlt: angles [deg]\n"
-             "  output.mrc: output file",
-             cxxopts::value<std::vector<std::string>>()) //
-            ("h,help", "Print help");
+void printargs(cxxopts::parse_result result) {
+	int const printwidth = 30;
+	std::cout << "Input files \n";
+	if (result.count("input")) {
+		std::cout << std::setw(printwidth) << "Input = " << result["input"].as<std::string>() << '\n';
+	}
+	if (result.count("angles")) {
+		std::cout << std::setw(printwidth) << "Angles = " << result["angles"].as<std::string>() << '\n';
+	}
+	if (result.count("output")) {
+		std::cout << std::setw(printwidth) << "Output = " << result["output"].as<std::string>() << '\n';
+	}
 
-        options.add_options("Alignment") //
-            ("t,transmission", "Projection images in [t]ransmission",
-             cxxopts::value<bool>()->default_value("true"))(
-                "canny", "sigma for Canny edge detection",
-                cxxopts::value<double>()->default_value("5.0")) //
-            ("sigma", "sigma for Common Line profile",
-             cxxopts::value<double>()->default_value("5.0")) //
-            ("c,capillary", "Thickness discarded for Common Line profile",
-             cxxopts::value<int>()->default_value("30")) //
-            ("pad", "Vertical padding for Common Line profile",
-             cxxopts::value<int>()->default_value("50")) //
-            ("b,bin", "[B]in output image", cxxopts::value<bool>()->default_value("true"));
-        options.parse_positional({"positional"});
+	std::cout << "Parameters \n";
+	print_arg<int>(result, "transmission", "Transmission File");
+	print_arg<double>(result, "canny", "Canny sigma");
+	print_arg<double>(result, "sigma", "Profile Sigma");
+	print_arg<int>(result, "capillary", "Capillary thickness");
+	print_arg<int>(result, "pad", "Y padding");
+	print_arg<int>(result, "bin", "Binning");
 
-        return options;
+	return;
+}
 
-    } catch (const cxxopts::option_error &e) {
-        std::cout << "error incxxopts_setup: " << e.what() << '\n';
-        std::cout << "Press Enter to Continue";
-        std::cin.ignore();
-        exit(1);
-    }
+
+cxxopts::options cxxopts_setup(char* const argv[]) {
+	try {
+		cxxopts::options options(argv[0], "One line description of MyProgram");
+
+		options.positional_help("projections.mrc angles.rawtlt output.mrc").show_positional_help();
+
+		options.add_options()    //
+			("input", ".mrc projection stack", cxxopts::value<std::string>())
+			("angles", ".rawtlt angles (degrees)", cxxopts::value<std::string>())
+			("output", ".mrc output file", cxxopts::value<std::string>())
+			("h,help", "Print help");
+
+
+		options.add_options("Alignment") //
+			("t,transmission", "Projection images in [t]ransmission",
+				cxxopts::value<int>()->default_value("1"))
+			("canny", "sigma for Canny edge detection",
+				cxxopts::value<double>()->default_value("5.0"))   //
+			("sigma", "sigma for Common Line profile",
+				cxxopts::value<double>()->default_value("5.0"))   //
+			("c,capillary", "Thickness discarded for Common Line profile",
+				cxxopts::value<int>()->default_value("30"))  //      
+			("pad", "Vertical padding for Common Line profile",
+				cxxopts::value<int>()->default_value("50"))  //      
+			("b,bin", "[B]in output image",
+				cxxopts::value<int>()->default_value("1"))
+			;
+		options.parse_positional({ "input", "angles", "output" });
+
+		return options;
+
+	}
+	catch (const cxxopts::option_error& e) {
+		std::cout << "error incxxopts_setup: " << e.what() << '\n';
+		std::cout << "Press Enter to Continue";
+		std::cin.ignore();
+		exit(1);
+	}
 }
 
 int main(int argc, char *argv[]) {
 
-    bool debug_mode = false;
-    std::vector<char *> argv_debug;
-    string base =
-        "c:/Users/axela/Documents/2023/SiriusXT/capillary/20230620_capillary_new_shroud_axel/";
-    string infile = (base + "20230620_capillary_new_shroud_tlt_flat_arec.mrc");
-    string inangle = (base + "20230620_capillary_new_shroud.rawtlt");
-    string outfile = (base + "20230620_capillary_new_shroud_tlt_flat_pre.mrc");
-    std::vector<std::string> arguments = {infile, inangle, outfile};
-
-    argv_debug.push_back(argv[0]);
-    for (const auto &arg : arguments)
-        argv_debug.push_back((char *)arg.data());
-    argv_debug.push_back(nullptr);
-
-    if (debug_mode) {
-        argc = 4;
-        argv = argv_debug.data();
-    }
     cxxopts::options options = cxxopts_setup(argv);
-
     try {
         cxxopts::parse_result parameters = options.parse(argc, argv);
 
@@ -100,29 +121,39 @@ int main(int argc, char *argv[]) {
             exit(0);
         }
 
-        if (parameters.count("positional") != 3) {
-            std::cout << "Invalid positional argumens!\n";
-            std::cout << "Got " << parameters.count("positional") << ", wanted 3!\n";
+		if (parameters.count("input") + parameters.count("angles") + parameters.count("output") != 3) {
+			std::cout << "Invalid positional argumens!\n";
 
-            std::cout << options.help() << '\n';
-            std::cout << "Press Enter to Continue";
-            std::cin.ignore();
-            exit(0);
-        }
+			std::cout << "Got following arguments:\n";
+			printargs(parameters);
 
-        std::vector<std::string> str_load = parameters["positional"].as<std::vector<std::string>>();
-        image_stack projections = slice_image(arecReadImage_stream(str_load[0]));
-        std::vector<double> angles = loadAngles(str_load[1]);
+			std::cout << options.help() << '\n';
+			std::cout << "Press Enter to Continue";
+			std::cin.ignore();
+			exit(0);
+		}
 
-        if (projections.size() == 0) {
-            cout << "Error in reading mrc file";
-            std::cout << str_load[0];
-            return 0;
-        }
-        if (angles.size() < 1) {
-            std::cout << "Failed to load angles.";
-            return 0;
-        }
+		std::cout << "Running Prealignment with arguments:\n\n";
+		printargs(parameters);
+		std::cout << "\n";
+
+
+		std::string input_file = parameters["input"].as<std::string>();
+		std::string angle_file = parameters["angles"].as<std::string>();
+		std::string output_file = parameters["output"].as<std::string>();
+		image_stack projections = slice_image(arecReadImage_stream(input_file));
+		std::vector<double> angles = loadAngles(angle_file);
+
+		if (projections.size() == 0) {
+			cout << "Error in reading mrc file";
+			std::cout << input_file;
+			return 0;
+		}
+		if (angles.size() < 1) {
+			std::cout << "Failed to load angles.";
+			std::cout << angle_file;
+			return 0;
+		}
         if (angles.size() != projections.size()) {
             std::cout << "Size of stack (";
             std::cout << projections.size();
@@ -139,7 +170,7 @@ int main(int argc, char *argv[]) {
         t.initialize_kernels(parameters["canny"].as<double>(), parameters["sigma"].as<double>());
         t.set_padding(parameters["capillary"].as<int>(), parameters["pad"].as<int>());
 
-        if (parameters["transmission"].as<bool>()) {
+        if (parameters["transmission"].as<int>()) {
             std::cout << "Preprocessing Transmission Images\n";
             preProcessing(projections);
         }
@@ -170,14 +201,14 @@ int main(int argc, char *argv[]) {
             M_final[i] = chain_transormations(M_final[i], M_trans);
         }
 
-        postProcessing(projections, t, parameters["transmission"].as<bool>(),
-                       parameters["bin"].as<bool>());
+        postProcessing(projections, t, parameters["transmission"].as<int>(),
+                       parameters["bin"].as<int>());
 
-        printf("Writing log, image and input file for AREC\n");
-        writePrealignment("prealign_log.txt", global_rot, sX, sX2, sY);
-        writeInput(str_load[2].c_str(), str_load[1].c_str());
-        writeAngles(str_load[1].c_str(), angles);
-        writeImage_stream(combine_stack(projections), str_load[2].c_str());
+		printf("Writing log, image and input file for AREC\n");
+		writePrealignment("prealign_log.txt", global_rot, sX, sX2, sY);
+		writeInput(output_file.c_str(), angle_file.c_str());
+		writeAngles(angle_file.c_str(), angles);
+		writeImage_stream(combine_stack(projections), output_file.c_str());
 
     } catch (const cxxopts::option_error &e) {
         std::cout << "error parsing options: " << e.what() << '\n';
